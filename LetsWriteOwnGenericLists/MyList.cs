6b541e2199075a15bdb7b2e6a9a2a1f78cddf798
Furkan Baser <furkanbaserceng@outlook.com>
2022-05-15 12:26:33 +0300

    Add project files.

---
+++ LetsWriteOwnGenericLists/MyList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsWriteOwnGenericLists
+{
+    public class MyList<T>
+    {
+        T[] arrays;
+
+        public MyList()
+        {
+            arrays = new T[0];
+        }
+
+        /// <summary>
+        /// Diziye yeni eleman ekleme metodu.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            T[] tempArray = arrays;             //newlenince referans kaybolmasın diziye geçiçi diziye aktarıldı.referanslar eşitlendi.
+            arrays = new T[arrays.Length + 1];  //eleman sayısı 1 arttı.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                arrays[i] = tempArray[i];       //yeni referans numarası alınıp eleman sayısı artırılmasına rağmen eski elemanları geçici diziden almış olduk.
+            }
+            arrays[arrays.Length - 1] = item;   //yeni eleman var olan diziye eklenmiş oldu.
+        }
+
+        /// <summary>
+        /// Dizinin eleman sayısını döndüren getter.
+        /// </summary>
+        public int MyLength
+        {
+            get { return arrays.Length; }
+        }
+
+        /// <summary>
+        /// Dizinin elemanlarını döndüren getter.
+        /// </summary>
+        public T[] Items
+        {
+            get { return arrays; }
+        }
+
+    }
+}
---
+++ LetsWriteOwnGenericLists/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsWriteOwnGenericLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(5);
+            myList.Add(10);
+
+            foreach (var item in myList.Items)
+            {
+                Console.WriteLine("Dizi elemanı:{0}",item);
+            }
+
+
+            Console.WriteLine("Dizinin eleman sayısı:{0}", myList.MyLength);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
